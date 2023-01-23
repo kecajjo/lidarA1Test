1155aee3646455e20f47d00ca9cb92879261e683
Jacek Multan <jul@spyro-soft.com>
2023-01-23 22:20:22 +0100

    lidar scanning in a loop

--- rpi/src/application/src/main.cpp
+++ rpi/src/application/src/main.cpp
@@ -1,9 +1,9 @@
+#include "LaserScan.pb.h"
+#include "publisher.hpp"
 #include "rplidar.hpp"
 #include <iostream>
 #include <string>
 #include <unistd.h>
-#include "publisher.hpp"
-#include "LaserScan.pb.h"
 
 #define PUBLISHED_TOPIC "LaserScan"
 
@@ -15,17 +15,19 @@ int main(int argc, char **argv) {
   Rplidar::Rplidar lidar(portName);
   auto pub = zmq_helper::Publisher<LaserScan>("tcp://*:5555");
 
-  lidar.startScan();
-  sleep(1);
-  auto data = lidar.getScanData(0);
+  while (1) {
+    lidar.startScan();
+    sleep(0.1);
+    auto data = lidar.getScanData(0);
 
-  LaserScan scan;
-  scan.set_size(data.size());
-  for (int i = 0; i < data.size(); i++) {
-    LaserScan::LaserRay* ray = scan.add_rays();
-    ray->set_quality(data[i].quality);
-    ray->set_angle_deg(data[i].angleDeg);
-    ray->set_range_mm(data[i].rangeMm);
+    LaserScan scan;
+    scan.set_size(data.size());
+    for (int i = 0; i < data.size(); i++) {
+      LaserScan::LaserRay *ray = scan.add_rays();
+      ray->set_quality(data[i].quality);
+      ray->set_angle_deg(data[i].angleDeg);
+      ray->set_range_mm(data[i].rangeMm);
+    }
+    pub.publish(scan, PUBLISHED_TOPIC);
   }
-  pub.publish(scan, PUBLISHED_TOPIC);
 }
